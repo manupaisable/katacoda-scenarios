30b0c101e5ae4fdf7cf334566486fccbe9182a01
Manuel Pais <manuel.pais@gmail.com>
2019-06-08 19:24:14 +0200

    need setup script to create folders in home dir

--- katacodacommanuelpaistrial/courseBase.sh
+++ katacodacommanuelpaistrial/courseBase.sh
@@ -3,3 +3,4 @@ mv ../jenkins-Dockerfile jenkins/
 
 cd ..
 git clone https://github.com/SkeltonThatcher/releasability-book.git
+mv -rf /home/scrapbook/releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
