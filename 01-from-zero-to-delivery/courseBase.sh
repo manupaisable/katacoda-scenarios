e6fcdeac9f4df07dad9ccd73da787c8bc292f7d8
Manuel Pais <manuel.pais@gmail.com>
2019-06-10 17:11:10 +0200

    zip not accepted, must use tar

--- 01-from-zero-to-delivery/courseBase.sh
+++ 01-from-zero-to-delivery/courseBase.sh
@@ -1,3 +1,5 @@
-cd /home/scrapbook/ 
-git clone https://github.com/SkeltonThatcher/releasability-book.git
-cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
+#cd /home/scrapbook/ 
+#git clone https://github.com/SkeltonThatcher/releasability-book.git
+#cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
+
+tar -xf demo-from-zero-to-delivery.tar
