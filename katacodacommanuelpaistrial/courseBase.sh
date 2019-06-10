fba60b48537cc6a3d14290d79159ef50e3cbadbd
Manuel Pais <manuel.pais@gmail.com>
2019-06-10 10:57:25 +0200

    fetching demo code from Github instead of uploading as assets

--- katacodacommanuelpaistrial/courseBase.sh
+++ katacodacommanuelpaistrial/courseBase.sh
@@ -1,3 +1,3 @@
-cd ..
+cd /home/scrapbook/ 
 git clone https://github.com/SkeltonThatcher/releasability-book.git
 cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
