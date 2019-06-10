9f71fc35ee568e3a3ce489ee90e519462fcdb10c
Manuel Pais <manuel.pais@gmail.com>
2019-06-10 17:39:11 +0200

    zip not accepted, must use tar

--- 01-from-zero-to-delivery/courseBase.sh
+++ 01-from-zero-to-delivery/courseBase.sh
@@ -2,4 +2,5 @@
 #git clone https://github.com/SkeltonThatcher/releasability-book.git
 #cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
 
+curl -LO https://katacoda.com/manuelpais/scenarios/01-from-zero-to-delivery/assets/demo-from-zero-to-delivery.tar
 tar -xf demo-from-zero-to-delivery.tar
