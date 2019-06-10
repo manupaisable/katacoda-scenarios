6549525dd5c0c2e5f572e09201a4a57293bcdd7e
Manuel Pais <manuel.pais@gmail.com>
2019-06-10 18:08:14 +0200

    trying to revert to working state

--- 01-from-zero-to-delivery/courseBase.sh
+++ 01-from-zero-to-delivery/courseBase.sh
@@ -1,3 +1,6 @@
-curl -LO https://katacoda.com/manuelpais/scenarios/01-from-zero-to-delivery/assets/demo-from-zero-to-delivery.tar
-tar -xf demo-from-zero-to-delivery.tar
-rm -f demo-from-zero-to-delivery.tar
+git clone https://github.com/SkeltonThatcher/releasability-book.git
+cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* .
+
+#curl -LO https://katacoda.com/manuelpais/scenarios/01-from-zero-to-delivery/assets/demo-from-zero-to-delivery.tar
+#tar -xf demo-from-zero-to-delivery.tar
+#rm -f demo-from-zero-to-delivery.tar
