cfddb35e82e2ba591f38cd4f9954c92bf3b70e18
Manuel Pais <manuel.pais@gmail.com>
2019-06-10 10:56:05 +0200

    fetching demo code from Github instead of uploading as assets

--- katacodacommanuelpaistrial/assets/pipeline-create.bat
+++
@@ -1,5 +0,0 @@
-set PUBLIC_IP_ADDRESS=192.168.99.100
-docker-compose down -v
-docker-compose rm -f -v
-docker-compose build
-docker-compose up -d
--- katacodacommanuelpaistrial/courseBase.sh
+++ katacodacommanuelpaistrial/courseBase.sh
@@ -1,6 +1,3 @@
-mkdir jenkins
-mv ../jenkins-Dockerfile jenkins/
-
 cd ..
 git clone https://github.com/SkeltonThatcher/releasability-book.git
-mv -rf /home/scrapbook/releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
+cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* tutorial/
